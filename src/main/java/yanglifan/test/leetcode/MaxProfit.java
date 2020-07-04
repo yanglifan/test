68fa659bc22b72585d6dafd6bfa5e667e2a35d26
Yang Lifan <yanglifan@gmail.com>
2020-07-04 09:51:44 +0800

    完成题目《买卖股票的最佳时机 II》

---
+++ src/main/java/yanglifan/test/leetcode/MaxProfit.java
@@ -0,0 +1,16 @@
+package yanglifan.test.leetcode;
+
+/**
+ * https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/1/array/22/
+ */
+public class MaxProfit {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) {
+                profit += prices[i] - prices[i - 1];
+            }
+        }
+        return profit;
+    }
+}
