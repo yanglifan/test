15fd5a1c8bf49f8f21009bd515e6220b608e2307
Yang Lifan <yanglifan@gmail.com>
2020-07-04 10:12:27 +0800

    完成题目《旋转数组》

---
+++ src/main/java/yanglifan/test/leetcode/Rotate.java
@@ -0,0 +1,17 @@
+package yanglifan.test.leetcode;
+
+/**
+ * https://leetcode-cn.com/explore/featured/card/top-interview-questions-easy/1/array/23/
+ */
+public class Rotate {
+    public void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        for (int i = 0; i < k; i++) {
+            int tmp = nums[nums.length - 1];
+            for (int j = nums.length - 1; j > 0; j--) {
+                nums[j] = nums[j - 1];
+            }
+            nums[0] = tmp;
+        }
+    }
+}
