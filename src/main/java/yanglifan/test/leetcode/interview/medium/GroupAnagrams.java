9cfb5933352a467aa054d50d6d29683e53f7b448
Yang Lifan <yanglifan@gmail.com>
2020-08-27 10:56:12 +0800

    完成《字母异位词分组》问题 https://leetcode-cn.com/problems/group-anagrams/solution/zi-mu-yi-wei-ci-fen-zu-by-leetcode/

---
+++ src/main/java/yanglifan/test/leetcode/interview/medium/GroupAnagrams.java
@@ -0,0 +1,28 @@
+package yanglifan.test.leetcode.interview.medium;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ */
+public class GroupAnagrams {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return new ArrayList<>();
+        }
+
+        Map<String, List<String>> results = new HashMap<>();
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String key = String.valueOf(chars);
+            if (!results.containsKey(key)) results.put(key, new ArrayList<>());
+            results.get(key).add(str);
+        }
+        return new ArrayList<>(results.values());
+    }
+}
