9726af1dc91bba2ad7967e40134b901dfc8e175d
Yang Lifan <yanglifan@gmail.com>
2020-08-31 11:56:57 +0800

    通过《括号生成》

--- src/main/java/yanglifan/test/leetcode/interview/medium/GenerateParenthesis.java
+++ src/main/java/yanglifan/test/leetcode/interview/medium/GenerateParenthesis.java
@@ -2,9 +2,8 @@ package yanglifan.test.leetcode.interview.medium;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.Set;
 
 /**
  * 括号生成，https://leetcode-cn.com/problems/generate-parentheses/
@@ -19,17 +18,22 @@ public class GenerateParenthesis {
             return Collections.singletonList("()");
         }
 
-        Set<String> results = new HashSet<>();
-        results.add("()");
+        List<List<String>> all = new LinkedList<>();
+        all.add(Collections.singletonList(""));
+        all.add(Collections.singletonList("()"));
         for (int i = 2; i <= n; i++) {
-            Set<String> newResults = new HashSet<>();
-            for (String base : results) {
-                newResults.add("(" + base + ")");
-                newResults.add("()" + base);
-                newResults.add(base + "()");
+            List<String> temp = new LinkedList<>();
+            for (int j = 0; j < i; j++) {
+                List<String> l1 = all.get(j);
+                List<String> l2 = all.get(i - j - 1);
+                for (String s1 : l1) {
+                    for (String s2 : l2) {
+                        temp.add("(" + s1 + ")" + s2);
+                    }
+                }
             }
-            results = newResults;
+            all.add(temp);
         }
-        return new ArrayList<>(results);
+        return all.get(n);
     }
 }
